d551063d8ef3f90d6963acf73ab1f4ec0defe43b
Lars <lars.oksendal@gmail.com>
2023-09-01 03:00:51 +0200

    first commit

---
+++ install_basesystem.sh
@@ -0,0 +1,378 @@
+#!/usr/bin/env -S bash -e
+
+# Fixing annoying issue that breaks GitHub Actions
+# shellcheck disable=SC2001
+
+# Cleaning the TTY.
+clear
+
+# Cosmetics (colours for text).
+BOLD='\e[1m'
+BRED='\e[91m'
+BBLUE='\e[34m'  
+BGREEN='\e[92m'
+BYELLOW='\e[93m'
+RESET='\e[0m'
+
+# Pretty print (function).
+info_print () {
+    echo -e "${BOLD}${BGREEN}[ ${BYELLOW}•${BGREEN} ] $1${RESET}"
+}
+
+# Pretty print for input (function).
+input_print () {
+    echo -ne "${BOLD}${BYELLOW}[ ${BGREEN}•${BYELLOW} ] $1${RESET}"
+}
+
+# Alert user of bad input (function).
+error_print () {
+    echo -e "${BOLD}${BRED}[ ${BBLUE}•${BRED} ] $1${RESET}"
+}
+
+# Installing the chosen networking method to the system (function).
+network_installer () {
+    info_print "Installing and enabling NetworkManager."
+    pacstrap /mnt networkmanager >/dev/null
+    systemctl enable NetworkManager --root=/mnt &>/dev/null
+}
+
+# User enters a password for the LUKS Container (function).
+lukspass_selector () {
+    input_print "Please enter a password for the LUKS container (you're not going to see the password): "
+    read -r -s password
+    if [[ -z "$password" ]]; then
+        echo
+        error_print "You need to enter a password for the LUKS Container, please try again."
+        return 1
+    fi
+    echo
+    input_print "Please enter the password for the LUKS container again (you're not going to see the password): "
+    read -r -s password2
+    echo
+    if [[ "$password" != "$password2" ]]; then
+        error_print "Passwords don't match, please try again."
+        return 1
+    fi
+    return 0
+}
+
+# Setting up a password for the user account (function).
+userpass_selector () {
+    input_print "Please enter name for a user account: "
+    read -r username
+    if [[ -z "$username" ]]; then
+        return 0
+    fi
+    input_print "Please enter a password for $username (you're not going to see the password): "
+    read -r -s userpass
+    if [[ -z "$userpass" ]]; then
+        echo
+        error_print "You need to enter a password for $username, please try again."
+        return 1
+    fi
+    echo
+    input_print "Please enter the password again (you're not going to see it): " 
+    read -r -s userpass2
+    echo
+    if [[ "$userpass" != "$userpass2" ]]; then
+        echo
+        error_print "Passwords don't match, please try again."
+        return 1
+    fi
+    return 0
+}
+
+# Microcode detector (function).
+microcode_detector () {
+    CPU=$(grep vendor_id /proc/cpuinfo)
+    if [[ "$CPU" == *"AuthenticAMD"* ]]; then
+        info_print "An AMD CPU has been detected, the AMD microcode will be installed."
+        microcode="amd-ucode"
+    else
+        info_print "An Intel CPU has been detected, the Intel microcode will be installed."
+        microcode="intel-ucode"
+    fi
+}
+
+# User enters a hostname (function).
+hostname_selector () {
+    input_print "Please enter the hostname Eg. OfficePC: "
+    read -r hostname
+    if [[ -z "$hostname" ]]; then
+        error_print "You need to enter a hostname in order to continue. Eg. OfficePC"
+        return 1
+    fi
+    return 0
+}
+
+# User chooses the locale (function).
+locale_selector () {
+    input_print "Please insert the locale you use (format: xx_XX. Enter empty to use en_US, or \"/\" to search locales): " locale
+    read -r locale
+    case "$locale" in
+        '') locale="en_US.UTF-8"
+            info_print "$locale will be the default locale."
+            return 0;;
+        '/') sed -E '/^# +|^#$/d;s/^#| *$//g;s/ .*/ (Charset:&)/' /etc/locale.gen | less -M
+                clear
+                return 1;;
+        *)  if ! grep -q "^#\?$(sed 's/[].*[]/\\&/g' <<< "$locale") " /etc/locale.gen; then
+                error_print "The specified locale doesn't exist or isn't supported."
+                return 1
+            fi
+            return 0
+    esac
+}
+
+# User chooses the console keyboard layout (function).
+keyboard_selector () {
+    input_print "Please insert the keyboard layout to use in console (Enter empty to use the Norwegian layout, type us for international english or \"/\" to look up for keyboard layouts): "
+    read -r kblayout
+    case "$kblayout" in
+        '') kblayout="no-latin1"
+            info_print "The standard Norwegian keyboard layout will be used."
+            return 0;;
+        '/') localectl list-keymaps
+             clear
+             return 1;;
+        *) if ! localectl list-keymaps | grep -Fxq "$kblayout"; then
+               error_print "The specified keymap doesn't exist."
+               return 1
+           fi
+        info_print "Changing console layout to $kblayout."
+        loadkeys "$kblayout"
+        return 0
+    esac
+}
+
+# Welcome screen.
+echo -ne "${BOLD}${BYELLOW}
+======================================================================
+
+██╗  ██╗██╗   ██╗██████╗ ██████╗        █████╗ ██████╗  ██████╗██╗  ██╗
+██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗      ██╔══██╗██╔══██╗██╔════╝██║  ██║
+███████║ ╚████╔╝ ██████╔╝██████╔╝█████╗███████║██████╔╝██║     ███████║
+██╔══██║  ╚██╔╝  ██╔═══╝ ██╔══██╗╚════╝██╔══██║██╔══██╗██║     ██╔══██║
+██║  ██║   ██║   ██║     ██║  ██║      ██║  ██║██║  ██║╚██████╗██║  ██║
+╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝      ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+                                                                       
+======================================================================
+${RESET}"
+info_print "Welcome to the first part of installing Hypr-Arch!"
+
+# Setting up keyboard layout.
+until keyboard_selector; do : ; done
+
+# Choosing the target for the installation.
+info_print "Available disks for the installation:"
+lsblk
+PS3="Please select the number of the corresponding disk (e.g. 1): "
+select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
+do
+    DISK="$ENTRY"
+    info_print "Arch Linux will be installed on the following disk: $DISK"
+    break
+done
+
+# Setting up LUKS password.
+until lukspass_selector; do : ; done
+
+# User choses the locale.
+until locale_selector; do : ; done
+
+# User choses the hostname.
+until hostname_selector; do : ; done
+
+# User sets up the user/root passwords.
+until userpass_selector; do : ; done
+
+# Warn user about deletion of old partition scheme.
+input_print "WARNING! This will wipe the current partition table on $DISK once installation starts. Do you agree [y/N]?: "
+read -r disk_response
+if ! [[ "${disk_response,,}" =~ ^(yes|y)$ ]]; then
+    error_print "Quitting."
+    exit
+fi
+info_print "Wiping $DISK."
+wipefs -af "$DISK" &>/dev/null
+sgdisk -Zo "$DISK" &>/dev/null
+
+# Creating a new partition scheme.
+info_print "Creating the partitions on $DISK."
+parted -s "$DISK" \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 513MiB \
+    set 1 esp on \
+    mkpart CRYPTROOT 513MiB 100% \
+
+ESP="/dev/disk/by-partlabel/ESP"
+CRYPTROOT="/dev/disk/by-partlabel/CRYPTROOT"
+
+# Informing the Kernel of the changes.
+info_print "Informing the Kernel about the disk changes."
+partprobe "$DISK"
+
+# Formatting the ESP as FAT32.
+info_print "Formatting the EFI Partition as FAT32."
+mkfs.fat -F 32 "$ESP" &>/dev/null
+
+# Creating a LUKS Container for the root partition.
+info_print "Creating LUKS Container for the root partition."
+echo -n "$password" | cryptsetup luksFormat "$CRYPTROOT" -d - &>/dev/null
+echo -n "$password" | cryptsetup open "$CRYPTROOT" cryptroot -d - 
+BTRFS="/dev/mapper/cryptroot"
+
+# Formatting the LUKS Container as BTRFS.
+info_print "Formatting the LUKS container as BTRFS."
+mkfs.btrfs "$BTRFS" &>/dev/null
+mount "$BTRFS" /mnt
+
+# Creating BTRFS subvolumes.
+info_print "Creating BTRFS subvolumes."
+subvols=(snapshots var_pkgs var_log home root srv)
+for subvol in '' "${subvols[@]}"; do
+    btrfs su cr /mnt/@"$subvol" &>/dev/null
+done
+
+# Mounting the newly created subvolumes.
+umount /mnt
+info_print "Mounting the newly created subvolumes."
+mountopts="ssd,noatime,compress-force=zstd:3,discard=async"
+mount -o "$mountopts",subvol=@ "$BTRFS" /mnt
+mkdir -p /mnt/{home,root,srv,.snapshots,var/{log,cache/pacman/pkg},boot}
+for subvol in "${subvols[@]:2}"; do
+    mount -o "$mountopts",subvol=@"$subvol" "$BTRFS" /mnt/"${subvol//_//}"
+done
+chmod 750 /mnt/root
+mount -o "$mountopts",subvol=@snapshots "$BTRFS" /mnt/.snapshots
+mount -o "$mountopts",subvol=@var_pkgs "$BTRFS" /mnt/var/cache/pacman/pkg
+chattr +C /mnt/var/log
+mount "$ESP" /mnt/boot/
+
+# Checking the microcode to install.
+microcode_detector
+
+# Pacstrap (setting up a base sytem onto the new root).
+info_print "Installing the base system (it may take a while)."
+pacstrap -K /mnt base base-devel linux-zen "$microcode" linux-firmware linux-zen-headers git vim btrfs-progs xdg-user-dirs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo &>/dev/null
+
+# Setting up the hostname.
+echo "$hostname" > /mnt/etc/hostname
+
+# Generating /etc/fstab.
+info_print "Generating a new fstab."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Configure selected locale and console keymap
+sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
+echo "LANG=$locale" > /mnt/etc/locale.conf
+echo "KEYMAP=$kblayout" > /mnt/etc/vconsole.conf
+
+# Setting hosts file.
+info_print "Setting hosts file."
+cat > /mnt/etc/hosts <<EOF
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain   $hostname
+EOF
+
+# Setting up the network.
+network_installer
+
+# Configuring /etc/mkinitcpio.conf.
+info_print "Configuring /etc/mkinitcpio.conf."
+cat > /mnt/etc/mkinitcpio.conf <<EOF
+HOOKS=(systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems)
+EOF
+
+# Setting up LUKS2 encryption in grub.
+info_print "Setting up grub config."
+UUID=$(blkid -s UUID -o value $CRYPTROOT)
+sed -i "\,^GRUB_CMDLINE_LINUX=\"\",s,\",&rd.luks.name=$UUID=cryptroot root=$BTRFS," /mnt/etc/default/grub
+
+# Configuring the system.
+info_print "Configuring the system (timezone, system clock, initramfs, Snapper, GRUB)."
+arch-chroot /mnt /bin/bash -e <<EOF
+
+    # Setting up timezone.
+    ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime &>/dev/null
+
+    # Setting up clock.
+    hwclock --systohc
+
+    # Generating locales.
+    locale-gen &>/dev/null
+
+    # Generating a new initramfs.
+    mkinitcpio -P &>/dev/null
+
+    # Snapper configuration.
+    umount /.snapshots
+    rm -r /.snapshots
+    snapper --no-dbus -c root create-config /
+    btrfs subvolume delete /.snapshots &>/dev/null
+    mkdir /.snapshots
+    mount -a &>/dev/null
+    chmod 750 /.snapshots
+
+    # Installing GRUB.
+    grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB &>/dev/null
+
+    # Creating grub config file.
+    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
+
+EOF
+
+# Setting user password.
+if [[ -n "$username" ]]; then
+    echo "%wheel ALL=(ALL:ALL) ALL" > /mnt/etc/sudoers.d/wheel
+    info_print "Adding the user $username to the system with root privilege."
+    arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$username"
+    info_print "Setting user password for $username."
+    echo "$username:$userpass" | arch-chroot /mnt chpasswd
+fi
+
+# Boot backup hook.
+info_print "Configuring /boot backup when pacman transactions are made."
+mkdir /mnt/etc/pacman.d/hooks
+cat > /mnt/etc/pacman.d/hooks/50-bootbackup.hook <<EOF
+[Trigger]
+Operation = Upgrade
+Operation = Install
+Operation = Remove
+Type = Path
+Target = usr/lib/modules/*/vmlinuz
+
+[Action]
+Depends = rsync
+Description = Backing up /boot...
+When = PostTransaction
+Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
+EOF
+
+# ZRAM configuration.
+info_print "Configuring ZRAM."
+cat > /mnt/etc/systemd/zram-generator.conf <<EOF
+[zram0]
+zram-size = min(ram, 8192)
+EOF
+
+# Pacman eye-candy features.
+info_print "Enabling colours, animations, and parallel downloads for pacman."
+sed -Ei 's/^#(Color)$/\1\nILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /mnt/etc/pacman.conf
+
+# Enabling various services.
+info_print "Enabling Reflector, automatic snapshots, BTRFS scrubbing and systemd-oomd."
+services=(reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd)
+for service in "${services[@]}"; do
+    systemctl enable "$service" --root=/mnt &>/dev/null
+done
+
+# Finishing up.
+info_print "Almost done!"
+info_print "1. Type reboot and hit enter to reboot the system."
+info_print "2. Log in with your user and password."
+info_print "3. Download the hypr-arch git repo (git clone https://github.com/aCeTotal/hypr-arch.git)."
+info_print "4. cd hypr-arch and ./install_hyprland.sh (You may first need to: sudo chmod +x install_hyprland.sh)"
+info_print "GOOD LUCK! :)"
+exit
---
+++ install_hyprland.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# A Simple script to install [aCe]Total's version of Arch Linux with Hyprland. 
+#
+
+# Install Yay
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si --noconfirm
+cd .. && rm -rf yay
+yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+
+# Adding the dotfiles
+cp -r dotfiles/* ~/.config
+
+# Clear the TTY
+clear
+
+# Install Packages
+yay -Syu --noconfirm \
+sddm qt5-wayland qt5ct waybar wofi xdg-desktop-portal-hyprland qt6-wayland qt6ct qt5-svg qt5-quickcontrols2 qt5-graphicaleffects gtk3 \
+polkit-gnome pipewire pipewire-pulse pipewire-jack wireplumber jq wl-clipboard cliphist timeshift wat-git rmlint rebuild-detector downgrade informant \
+python-requests pacman-contrib lxappearance file-roller gvfs thunar thunar-archive-plugin bluez bluez-utils pavucontrol pamixer btop mpv \
+network-manager-applet blueman grim slurp swappy linux-headers \
+steam gamescope discord gamemode mangohud lutris wine-staging protonup-qt \
+alacritty brave-bin rider blender gimp \
+icaclient nfs-utils network-manager-sstp sstp-client \
+ttf-jetbrains-mono-nerd papirus-icon-theme \
+
+# Disable WIFI Powersave
+echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a /etc/NetworkManager/conf.d/wifi-powersave.conf
+sudo systemctl restart NetworkManager
+
+# Check if NVIDIA GPU is found
+if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
+   yay -Syu --noconfirm nvidia-dkms libva libva-nvidia-driver hyprland-nvidia
+
+   echo -e "\n MODULES=(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm)" | sudo tee -a /etc/mkinitcpio.conf
+   echo -e "options nvidia-drm modeset=1\noptions nvidia NVreg_UsePageAttributeTable=1\noptions nvidia NVreg_EnableResizableBar=1\noptions nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"" | sudo tee -a /etc/modprobe.d/nvidia.conf
+   sudo mkinitcpio -P
+
+   # Adding Pacman hook to update initramfs after Nvidia driver upgrade
+   echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia-dkms\nTarget=usr/lib/modules/*/vmlinuz\n\n[Action]\nDescription=Update NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'" | sudo tee -a /etc/pacman.d/hooks/nvidia.hook
+
+   sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3" /GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"/' /etc/default/grub
+   sudo sed -i 's/GRUB_TIMEOUT=5 /GRUB_TIMEOUT=1/' /etc/default/grub 
+   sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+else
+   yay -Syu --noconfirm hyprland
+
+   echo -e "\nMODULES=(btrfs)" | sudo tee -a /etc/mkinitcpio.conf
+   sudo mkinitcpio -P
+fi
+
+# Adding theme for SDDM
+git clone https://github.com/ArtemSmaznov/SDDM-themes.git
+cd SDDM-themes
+sudo cp -r deepin/ /usr/share/sddm/themes/
+sudo sed -i "s/^Current=.*/Current=deepin/g" /etc/sddm.conf
+
+# Enable services
+sudo systemctl enable bluetooth.service
+sudo systemctl enable sddm
+
+# Creating the Hyprland wayland session
+sudo mkdir -p /usr/share/wayland-sessions
+echo -e "[Desktop Entry]\nName=Hyprland\nComment=An intelligent dynamic tiling Wayland compositor\nExec=Hyprland\nType=Application" | sudo tee -a /usr/share/wayland-sessions/hyprland.conf
+
+# Removing install files and reboot the system
+cd && rm -rf hypr-arch
+reboot
+
+
