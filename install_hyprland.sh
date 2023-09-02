bf2ac431b131d8131a1b7d9af39c2e26137b3193
Lars <lars.oksendal@gmail.com>
2023-09-03 00:58:51 +0200

    fixing som sed bugs

--- install_hyprland.sh
+++ install_hyprland.sh
@@ -16,21 +16,26 @@ sudo pacman -Syu --noconfirm
 # Adding the dotfiles
 cp -r dotfiles/* ~/.config
 
+# Moving default wallpaper to /usr/share/wallpapers
+sudo mkdir -p /usr/share/wallpapers/
+sudo cp dotfiles/wallpapers/* /usr/share/wallpapers/
+
 # Clear the TTY
 clear
 
 # Install Packages
 yay -Syu --noconfirm \
 sddm qt5-wayland qt5ct waybar wofi xdg-desktop-portal-hyprland qt6-wayland qt6ct qt5-svg qt5-quickcontrols2 qt5-graphicaleffects gtk3 \
-polkit-gnome pipewire pipewire-pulse pipewire-jack wireplumber jq wl-clipboard cliphist timeshift wat-git rmlint rebuild-detector downgrade \
+polkit-gnome pipewire swaybg pipewire-pulse pipewire-jack wireplumber jq wl-clipboard cliphist timeshift wat-git rmlint rebuild-detector downgrade \
 python-requests pacman-contrib lxappearance file-roller gvfs thunar thunar-archive-plugin bluez bluez-utils pavucontrol pamixer btop mpv \
-network-manager-applet blueman grim slurp swappy linux-headers \
+network-manager-applet blueman grim slurp swappy linux-headers firewalld fail2ban \
 steam gamescope discord gamemode mangohud lutris wine-staging protonup-qt \
-alacritty brave-bin rider blender gimp \
-icaclient nfs-utils network-manager-sstp sstp-client \
+alacritty firefox rider blender pureref gimp \
+icaclient nfs-utils network-manager-sstp sstp-client github-cli \
 ttf-jetbrains-mono-nerd papirus-icon-theme \
 
 # Disable WIFI Powersave
+sudo touch /etc/NetworkManager/conf.d/wifi-powersave.conf
 echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a /etc/NetworkManager/conf.d/wifi-powersave.conf
 sudo systemctl restart NetworkManager
 
@@ -41,11 +46,34 @@ if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
    sudo mkdir -p /etc/modprobe.d
    sudo touch /etc/modprobe.d/nvidia.conf
    echo -e "\nMODULES=(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm)" | sudo tee -a /etc/mkinitcpio.conf
-   echo -e "options nvidia-drm modeset=1\noptions nvidia NVreg_UsePageAttributeTable=1\noptions nvidia NVreg_EnableResizableBar=1\noptions nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"" | sudo tee -a /etc/modprobe.d/nvidia.conf
+   
+   sudo tee /etc/modprobe.d/nvidia.conf > /dev/null <<'TXT'
+   options nvidia-drm modeset=1
+   options nvidia NVreg_UsePageAttributeTable=1
+   options nvidia NVreg_EnablePCIeGen3=1
+   options nvidia NVreg_EnableResizableBar=1
+   options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"
+TXT
+   
    sudo mkinitcpio -P
 
    # Adding Pacman hook to update initramfs after Nvidia driver upgrade
-   echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia-dkms\nTarget=usr/lib/modules/*/vmlinuz\n\n[Action]\nDescription=Update NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'" | sudo tee -a /etc/pacman.d/hooks/nvidia.hook
+   sudo tee /etc/pacman.d/hooks/nvidia.hook > /dev/null <<'TXT'
+  [Trigger]
+  Operation=Install
+  Operation=Upgrade
+  Operation=Remove
+  Type=Package
+  Target=nvidia-dkms
+  Target=linux-zen
+
+  [Action]
+  Description=Update NVIDIA module in initcpio
+  Depends=mkinitcpio
+  When=PostTransaction
+  NeedsTargets
+  Exec=/bin/sh -c 'while read -r trg; do case $trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+TXT
 
    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"/g' /etc/default/grub
    sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub 
@@ -55,18 +83,22 @@ else
    yay -Syu --noconfirm hyprland
 
    echo -e "\nMODULES=(btrfs)" | sudo tee -a /etc/mkinitcpio.conf
-   sudo mkinitcpio -P
+   sudo grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
 # Adding theme for SDDM
 git clone https://github.com/ArtemSmaznov/SDDM-themes.git
 cd SDDM-themes
 sudo cp -r deepin/ /usr/share/sddm/themes/
-sudo sed -i "s/^Current=.*/Current=deepin/g" /etc/sddm.conf
+sudo mkdir -p /etc/sddm.conf.d/
+sudo cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/default.conf
+sudo sed -i "s/^Current=/Current=deepin/g" /etc/sddm.conf.d/default.conf
 
 # Enable services
 sudo systemctl enable bluetooth.service
 sudo systemctl enable sddm
+sudo systemctl enable firewalld.service # https://wiki.archlinux.org/title/Firewalld
+sudo systemctl enable fail2ban.service # https://wiki.archlinux.org/title/Fail2ban
 
 # Creating the Hyprland wayland session
 sudo mkdir -p /usr/share/wayland-sessions
