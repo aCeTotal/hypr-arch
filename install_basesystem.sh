4ba75c6f8c03cc7ff0bb8843ab5d003a6171f3ab
aCeTotal <lars.oksendal@gmail.com>
2024-07-25 13:29:18 +0200

    install

--- install_basesystem.sh
+++ install_basesystem.sh
@@ -332,7 +332,7 @@ cat > /mnt/boot/loader/entries/arch.conf <<EOF
 title   Arch Linux
 linux   /vmlinuz-linux-zen
 initrd  /initramfs-linux-zen.img
-options cryptdevice=PARTUUID=$PARTUUID:cryptroot root=$BTRFS rootflags=subvol=@ rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
+options cryptdevice=PARTUUID=$PARTUUID:cryptroot root=$BTRFS rootflags=subvol=@ rw quiet loglevel=3
 EOF
 
 cat > /mnt/boot/loader/loader.conf <<EOF
